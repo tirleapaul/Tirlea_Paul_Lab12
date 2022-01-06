11f1e38c6bca9ada60810b113ae283a66f55559a
Paul Sebastian Bogdan Tirlea <Tirlea.Al.Paul@student.utcluj.ro>
2022-01-06 15:22:28 +0200

    Add project files.

---
+++ Tirlea_Paul_Lab12/Tirlea_Paul_Lab12/Data/IRestService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tirlea_Paul_Lab12.Models;
+
+namespace Tirlea_Paul_Lab12.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
